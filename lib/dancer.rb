c94e839ef975dfccd11416fad786c6363d727eaa
Brandon Furr <brandon.furr123@gmail.com>
2019-10-18 02:39:47 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include FancyDance::ClassMethods
+  include 
   extend 
   
   attr_accessor :name 
